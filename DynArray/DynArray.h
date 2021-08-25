048dc17fb1e4e8f06249da9ce20ab012480058fe
James Taylor <james.taylor_rw@outlook.com>
2021-08-25 17:26:02 +0800

    Add files via upload

--- DynArray/DynArray.h
+++ DynArray/DynArray.h
@@ -12,28 +12,26 @@ template<typename T, size_t Dimension>
 class DynArrayRef
 {
 public:
-	DynArrayRef(T* data_, size_t* cur_dim_size_, size_t total) noexcept
-		: data(data_)
-		, cur_dim_size(cur_dim_size_)
-		, remain(total / *cur_dim_size_)
+	DynArrayRef(T* data_, size_t* sizes, size_t* remains) noexcept
+		: data(data_), size(sizes), remain(remains)
 	{}
 
 	auto operator[](size_t index) const noexcept
 	{
-		return DynArrayRef<T, Dimension - 1>(data + index * remain, cur_dim_size + 1, remain);
+		return DynArrayRef<T, Dimension - 1>(data + *remain * index, size + 1, remain + 1);
 	}
 
 private:
-	T* data;
-	size_t* cur_dim_size;
-	size_t remain;
+	T* const data;
+	size_t* const size;
+	size_t* const remain;
 };
 
 template<typename T>
 class DynArrayRef<T, 1>
 {
 public:
-	DynArrayRef(T* data_, size_t*, size_t) noexcept
+	DynArrayRef(T* data_, size_t*, size_t*) noexcept
 		: data(data_)
 	{}
 
@@ -48,7 +46,7 @@ public:
 	}
 
 private:
-	T* data;
+	T* const data;
 };
 // -------------------------------------------
 
@@ -66,6 +64,20 @@ namespace detail {
 	{
 		return GetTotalSizeAndFill(dim_info, size) * GetTotalSizeAndFill(dim_info + 1, sizes...);
 	}
+
+	inline auto NullptrAndFillRemains(size_t dimension, size_t* remains, const size_t* sizes) noexcept
+	{
+		remains += dimension - 1;
+		sizes += dimension - 1;
+		*remains-- = 1;
+		while (--dimension)
+		{
+			*remains = *sizes * remains[1];
+			remains--;
+			sizes--;
+		}
+		return nullptr;
+	}
 }
 
 // --------------- Main class DynArray ---------------
@@ -76,9 +88,10 @@ class DynArray
 public:
 	template<typename... Args>
 	DynArray(Args... sizes)
+		: total_count{ detail::GetTotalSizeAndFill(dim_info.sizes, sizes...) }
+		, data{ detail::NullptrAndFillRemains(Dimension, dim_info.remains, dim_info.sizes) }
 	{
 		static_assert(sizeof...(Args) == Dimension, "The specified Dimension is not equal to sizes args");
-		total_count = detail::GetTotalSizeAndFill(dim_info.sizes, sizes...);
 		data = new T[total_count]{}; // zero initialized
 	}
 	~DynArray() noexcept
@@ -87,9 +100,9 @@ public:
 	}
 	DynArray(const DynArray& other)
 	{
-		dim_info = other.dim_info;
-		total_count = other.total_count;
-		data = new T[total_count];
+		this->dim_info = other.dim_info;
+		this->total_count = other.total_count;
+		this->data = new T[total_count];
 		std::copy_n(other.data, total_count, this->data);
 	}
 	DynArray& operator=(const DynArray& other)&
@@ -113,19 +126,20 @@ public:
 	{
 		if (this == &other)
 			return *this;
-		~DynArray();
+		this->~DynArray();
 		// not checking each dimensions' size equal
 		// assign two different DynArrays is not recommended
 		this->data = other.data;
 		this->dim_info = other.dim_info;
 		this->total_count = other.total_count;
 		other.data = nullptr;
+		return *this;
 	}
 
 public:
 	auto ref() noexcept
 	{
-		return DynArrayRef<T, Dimension>(data, dim_info.sizes, total_count);
+		return DynArrayRef<T, Dimension>(data, dim_info.sizes, dim_info.remains);
 	}
 	decltype(auto) operator[](size_t index) noexcept
 	{
@@ -149,11 +163,12 @@ private:
 	}
 
 private:
-	T* data;
 	struct { // for assigning conveniently
 		size_t sizes[Dimension];
+		size_t remains[Dimension];
 	}dim_info;
 	size_t total_count;
+	T* data;
 };
 
 #endif // DYNARRAY_HEADER_
