4bcfb09db32b301c70ae6411ae45d24ca1e9fcf2
James Taylor <james.taylor_rw@outlook.com>
2023-09-21 00:10:21 +0800

    Add files via upload

--- Pythonic/range.h
+++ Pythonic/range.h
@@ -2,53 +2,91 @@
 
 #include <type_traits>
 #include <concepts>
+#include <cassert>
 
-template<std::signed_integral T = std::make_signed_t<size_t>>
+template<std::integral T>
 class range
 {
 public:
+	using difference_type = std::make_signed_t<T>;
+	using value_type = T;
+
 	class range_iterator
 	{
 	public:
-		constexpr range_iterator(T pos) noexcept :_pos(pos) {}
-		constexpr range_iterator(T pos, T step) noexcept
-			:_pos(pos), _step(step)
+		using difference_type = range::difference_type;
+		using value_type = range::value_type;
+	public:
+		constexpr range_iterator() = default;
+		constexpr range_iterator(value_type pos, difference_type step) noexcept
+			: pos_(pos), step_(step)
 		{}
-		constexpr T operator*() const noexcept { return _pos; }
-		constexpr T operator++() noexcept { return _pos += _step; }
-		constexpr T operator++(int) noexcept { return (_pos += _step) - _step; }
-		constexpr bool operator==(const range_iterator& rhs) const noexcept {
-			return this->_pos == rhs._pos;
-		}
-		constexpr bool operator!=(const range_iterator& rhs) const noexcept {
-			return !(*this == rhs);
+		constexpr value_type operator*() const noexcept { return pos_; }
+		constexpr range_iterator& operator++() noexcept { pos_ += step_; return *this; }
+		constexpr range_iterator operator++(int) noexcept { auto tmp = *this; ++*this; return tmp; }
+		constexpr range_iterator& operator--() noexcept { pos_ -= step_; return *this; }
+		constexpr range_iterator operator--(int) noexcept { auto tmp = *this; ++*this; return tmp; }
+		constexpr bool operator==(const range_iterator& rhs) const noexcept
+		{
+			return this->pos_ == rhs.pos_;
 		}
 	private:
-		T _pos;
-		T _step = T{ 1 };
+		value_type pos_ = value_type{ 0 };
+		difference_type step_ = difference_type{ 1 };
 	};
 
-	constexpr range(T end) noexcept :_end(end) { check(); }
-	constexpr range(T begin, T end) noexcept :_begin(begin), _end(end) { check(); }
-	constexpr range(T begin, T end, T step) noexcept
-		:_begin(begin), _end(end), _step(step) {
+	template<typename V>
+	constexpr range(V end) noexcept
+		: end_(static_cast<value_type>(end))
+	{
 		check();
 	}
-	constexpr auto begin() const noexcept {
-		return range_iterator(_begin, _step);
+	template<typename U, typename V>
+	constexpr range(U begin, V end) noexcept
+		: begin_(static_cast<value_type>(begin)), end_(static_cast<value_type>(end))
+	{
+		check();
 	}
-	constexpr auto end() const noexcept {
-		return range_iterator(_end, _step);
+	template<typename U, typename V, typename W>
+	constexpr range(U begin, V end, W step) noexcept
+		: begin_(static_cast<value_type>(begin)), end_(static_cast<value_type>(end))
+		, step_(static_cast<difference_type>(step))
+	{
+		check();
+	}
+	constexpr auto begin() const noexcept
+	{
+		return range_iterator(begin_, step_);
+	}
+	constexpr auto end() const noexcept
+	{
+		return range_iterator(end_, step_);
 	}
 
 private:
-	constexpr void check() noexcept {
-		if (_step > T{ 0 } && _begin > _end ||
-			_step < T{ 0 } && _begin < _end || _step == T{ 0 })
-			_begin = _end;
+	constexpr void check() noexcept
+	{
+		if (std::is_constant_evaluated())
+		{
+			if (step_ > difference_type{ 0 } && begin_ > end_ ||
+				step_ < difference_type{ 0 } && begin_ < end_ || step_ == difference_type{ 0 })
+				begin_ = end_;
+		}
+		else
+		{
+			assert(step_ > difference_type{ 0 } && begin_ <= end_ ||
+				   step_ < difference_type{ 0 } && begin_ >= end_);
+		}
 	}
 	// [begin, end)
-	T _begin = T{ 0 };
-	T _end;
-	T _step = T{ 1 };
+	value_type begin_ = value_type{ 0 };
+	value_type end_;
+	difference_type step_ = difference_type{ 1 };
 };
+
+template<typename V>
+range(V) -> range<V>;
+template<typename U, typename V>
+range(U, V) -> range<V>;
+template<typename U, typename V, typename W>
+range(U, V, W) -> range<V>;
