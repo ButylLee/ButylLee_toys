a7e43d758bacd909c8a805853e25a07fb086ad65
James Taylor <james.taylor_rw@outlook.com>
2023-10-03 22:30:25 +0800

    Add files via upload

---
+++ Property/Property.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+#ifndef PROPERTY_HEADER_
+#define PROPERTY_HEADER_
+
+#include <type_traits>
+#include <concepts>
+#include <utility>
+
+enum struct AccessLevel
+{
+	None, Get, Set, GetSet
+};
+
+namespace detail
+{
+	template<AccessLevel Level>
+	concept Getable = Level == AccessLevel::Get || Level == AccessLevel::GetSet;
+	template<AccessLevel Level>
+	concept Setable = Level == AccessLevel::Set || Level == AccessLevel::GetSet;
+	template<typename T, typename Arg>
+	constexpr bool CanNothrowInit =
+		std::is_nothrow_constructible_v<T, Arg>;
+	template<typename T, typename Arg>
+	constexpr bool CanNothrowSet =
+		std::is_nothrow_constructible_v<T, Arg>&&
+		std::is_nothrow_assignable_v<T&, T>;
+}
+
+template<typename Owner, typename T, AccessLevel Level = AccessLevel::None>
+class Property
+{
+	friend typename Owner;
+public:
+	using Type = std::decay_t<T>;
+	using GetType = const Type&;
+
+public:
+	Property() requires std::default_initializable<Type> = default;
+
+	template<typename V = Type>
+	explicit constexpr Property(V&& value) noexcept(detail::CanNothrowInit<Type, V>)
+		: value(std::forward<V>(value))
+	{}
+
+public:
+	constexpr GetType get() const noexcept requires detail::Getable<Level>
+	{
+		return value;
+	}
+
+	constexpr operator GetType() const noexcept requires detail::Getable<Level>
+	{
+		return value;
+	}
+
+	template<typename V = Type>
+	constexpr void set(V&& other) noexcept(detail::CanNothrowSet<Type, V>)
+		requires detail::Setable<Level>
+	{
+		value = Type(std::forward<V>(other));
+	}
+
+	template<typename V = Type>
+	constexpr Property& operator=(V&& other) noexcept(detail::CanNothrowSet<Type, V>)
+		requires detail::Setable<Level>
+	{
+		value = Type(std::forward<V>(other));
+		return *this;
+	}
+
+private:
+	Type value;
+};
+
+#define UsingProperty(OwnerType) \
+	using enum AccessLevel; \
+	template<typename T, AccessLevel Level> \
+	using Property = Property<OwnerType, T, Level>
+
+#endif // PROPERTY_HEADER_
---
+++ Property/test.cpp
@@ -0,0 +1,78 @@
+﻿#include "Property.h"
+#include <cassert>
+
+void f1(short value)
+{
+	(void)value;
+}
+
+struct Num1
+{
+	short i = 42;
+	operator short() const noexcept
+	{
+		return i;
+	}
+};
+
+struct Num2
+{
+	short i;
+	Num2(short v) :i(v) {}
+	operator short() const noexcept
+	{
+		return i;
+	}
+};
+
+class A
+{
+	UsingProperty(A);
+public:
+	Property<int, None> i1{ 1 };
+	Property<int, Get> i2{ 2 };
+	Property<int, Set> i3{ 3 };
+	Property<int, GetSet> i4{ 4 };
+	void foo()
+	{
+		i1.value = 10;
+		i2.value = 20;
+		i3.value = 30;
+		i4.value = 40;
+		assert(i1.value == 10);
+		assert(i2.value == 20);
+		assert(i3.value == 30);
+		assert(i4.value == 40);
+	}
+
+	Property<Num1, GetSet> num1;
+	Property<Num2, GetSet> num2{ 42 };
+	Property<Num2, GetSet> num3;
+	A() : num3(42) {}
+};
+
+int main()
+{
+	A a;
+	a.foo();
+
+	//a.i1 = 42;
+	//f1(a.i1);
+
+	//a.i2 = 42;
+	f1(a.i2);
+	f1(a.i2.get());
+
+	a.i3 = 42;
+	a.i3.set(24);
+	//f1(a.i3);
+
+	a.i4 = 42;
+	f1(a.i4);
+
+	a.num1 = 24;
+	f1(a.num1.get()); // use get() to cope with 2-step implicit cast
+
+	assert(a.num2.get() == 42);
+	assert(a.num3.get() == 42);
+}
